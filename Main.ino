ec609724c6a4afc107a0c02d89444416133cd750
el-shago <92878520+el-shago@users.noreply.github.com>
2024-02-26 20:41:04 -0800

    Si ola que tal

--- Main.ino
+++ Main.ino
@@ -1,5 +1,5 @@
 #include <WiFi101.h>
-#include "secrets.h"  // Importa las credenciales WiFi y API Key desde un archivo secreto
+#include "secrets.h"
 #include "ThingSpeak.h"
 #include <Adafruit_BMP085.h>
 #include "DHT.h"
@@ -9,26 +9,30 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-char ssid[] = SECRET_SSID;  // Nombre de la red WiFi
-char pass[] = SECRET_PASS;  // Contraseña de la red WiFi
+char ssid[] = SECRET_SSID;  
+char pass[] = SECRET_PASS;  
 WiFiClient client;
 
-unsigned long myChannelNumber = SECRET_CH_ID;        // Número de canal ThingSpeak
-const char *myWriteAPIKey = SECRET_WRITE_APIKEY;      // API Key para escritura en ThingSpeak
+unsigned long myChannelNumber = SECRET_CH_ID;        
+const char *myWriteAPIKey = SECRET_WRITE_APIKEY;      
 Adafruit_BMP085 bmp;
 
 unsigned long lastAverageTime = 0;
 float totalTemperature = 0.0;
 int readingsCount = 0;
+float totalPressure = 0.0;
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
+  
   while (!Serial);
+  
   if (!bmp.begin()) {
     Serial.println("Could not find a valid BMP085 sensor, check wiring!");
     while (1);
   }
+  
   Serial.println(WiFi.status());
   ThingSpeak.begin(client);
 }
@@ -37,29 +41,35 @@ void loop() {
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(SECRET_SSID);
+    
     while (WiFi.status() != WL_CONNECTED) {
       int status = WiFi.begin(ssid, pass);
       Serial.print(".");
       delay(10000);
     }
+    
     Serial.println("\nConnected.");
   }
 
   float temperatureBMP = bmp.readTemperature();
   float humidity = dht.readHumidity();
   float temperatureDHT = dht.readTemperature();
-  float altitude = bmp.readAltitude();
+  float pressure = bmp.readPressure();
 
   unsigned long currentTime = millis();
 
   if (currentTime - lastAverageTime >= 20000) {
+    // Calcula las medias de temperatura y presión
     float averageTemperature = totalTemperature / readingsCount;
+    float averagePressure = totalPressure / readingsCount;
 
     // Configura los campos para enviar a ThingSpeak
     ThingSpeak.setField(1, averageTemperature);
     ThingSpeak.setField(2, humidity);
     ThingSpeak.setField(3, temperatureDHT);
-    ThingSpeak.setField(4, altitude);
+    ThingSpeak.setField(4, temperatureBMP);
+    ThingSpeak.setField(5, pressure);
+    ThingSpeak.setField(6, averagePressure);
 
     // Envía los datos a ThingSpeak
     int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
@@ -71,11 +81,14 @@ void loop() {
     }
 
     // Reinicia variables para el próximo ciclo
+    totalPressure = 0.0;
     totalTemperature = 0.0;
     readingsCount = 0;
     lastAverageTime = currentTime;
   }
 
+  // Acumula los valores para calcular medias
+  totalPressure += pressure;
   totalTemperature += temperatureBMP;
   readingsCount++;
 
@@ -86,9 +99,8 @@ void loop() {
   Serial.println(humidity);
   Serial.print("Temperatura DHT11: ");
   Serial.println(temperatureDHT);
-  Serial.print("Altura BMP180: ");
-  Serial.println(altitude);
+  Serial.print("Presion BMP180: ");
+  Serial.println(pressure);
 
   delay(2000);
 }
-
--- loadData.py
+++ loadData.py
@@ -1,12 +1,12 @@
-import requests
 import os
 from dotenv import load_dotenv
+import requests
 import matplotlib.pyplot as plt
 from datetime import datetime
 
 load_dotenv()
 
-channel_id = os.getenv('CHID','')
+channel_id = os.getenv('CHID','')  
 read_api_key = os.getenv('READKEY','')
 
 url = f"https://api.thingspeak.com/channels/{channel_id}/feeds.json"
@@ -15,31 +15,40 @@ params = {"api_key": read_api_key}
 response = requests.get(url, params=params)
 
 data = response.json()["feeds"]
-
-temperatura_puntual = [float(entry['field1']) for entry in data]  # Temperatura puntual
-temperatura_promedio = [float(entry['field2']) for entry in data]  # Temperatura promedio
-humedad_puntual = [float(entry['field3']) for entry in data]             # Humedad puntual
-humedad_promedio = [float(entry['field4']) for entry in data]               # Humedad promedio
-
-# Gráficas
 timestamps = [datetime.strptime(entry['created_at'], "%Y-%m-%dT%H:%M:%SZ") for entry in data]
+temperature_promedio = [float(entry['field1']) for entry in data]  # Campo 1: Temperatura promedio
+humidity_puntual = [float(entry['field2']) for entry in data]      # Campo 2: Humedad puntual
+temperature_dht11 = [float(entry['field3']) for entry in data]      # Campo 3: Temperatura DHT11
+temperature_bmp180 = [float(entry['field4']) for entry in data]     # Campo 4: Temperatura BMP180
+pressure_puntual = [float(entry['field5']) if entry['field5'] is not None else 0.0 for entry in data]  # Campo 5: Presión puntual BMP180
+pressure_promedio = [float(entry['field6']) if entry['field6'] is not None else 0.0 for entry in data]  # Campo 6: Presión promedio BMP180
 
-plt.figure(figsize=(10, 6))
 
-plt.subplot(2, 1, 1)
-plt.plot(timestamps, temperatura_promedio, label="Temperatura Puntual")
-plt.plot(timestamps, temperatura_puntual, label="Temperatura Promedio")
+# Crear gráficas
+plt.figure(figsize=(10, 8))
+
+plt.subplot(3, 1, 1)
+plt.plot(timestamps, temperature_promedio, label="Temperatura Promedio")
+plt.plot(timestamps, temperature_dht11, label="Temperatura DHT11")
+plt.plot(timestamps, temperature_bmp180, label="Temperatura BMP180")
 plt.title('Comparativa de Temperatura')
 plt.xlabel('Tiempo')
 plt.ylabel('Temperatura (°C)')
 plt.legend()
 
-plt.subplot(2, 1, 2)
-plt.plot(timestamps, humedad_promedio, label="Humedad Puntual")
-plt.plot(timestamps, humedad_puntual, label="Humedad Promedio")
-plt.title('Comparativa de Humedad')
+plt.subplot(3, 1, 2)
+plt.plot(timestamps, humidity_puntual, label="Humedad Puntual")
+plt.title('Humedad')
 plt.xlabel('Tiempo')
 plt.ylabel('Humedad (%)')
+plt.legend()
+
+plt.subplot(3, 1, 3)
+plt.plot(timestamps, pressure_promedio, label="Presión Promedio")
+plt.plot(timestamps, pressure_puntual, label="Presión Puntual")
+plt.title('Comparativa de Presión')
+plt.xlabel('Tiempo')
+plt.ylabel('Presión (Pa)')
 plt.legend()
 
 plt.tight_layout()
